4a0bfc3718addf7aaae05642ccf78c012a02c124
Saefina Bahari <s.fyna123@gmail.com>
2025-10-30 21:38:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
